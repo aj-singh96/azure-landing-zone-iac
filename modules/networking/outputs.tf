dc36d3f1f2ed74adc639c34ce30c33be3589eeb6
aj-singh96 <72389457+aj-singh96@users.noreply.github.com>
2025-10-23 16:43:25 +0530

    Update outputs.tf

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,1 +1,24 @@
-# Outputs for networking
+output "vnet_id" {
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.this.id
+}
+
+output "vnet_name" {
+  description = "The name of the virtual network"
+  value       = azurerm_virtual_network.this.name
+}
+
+output "subnet_ids" {
+  description = "Map of subnet names to their IDs"
+  value       = { for k, v in azurerm_subnet.subnets : k => v.id }
+}
+
+output "subnet_address_prefixes" {
+  description = "Map of subnet names to their address prefixes"
+  value       = { for k, v in azurerm_subnet.subnets : k => v.address_prefixes }
+}
+
+output "nsg_ids" {
+  description = "Map of NSG names to their IDs"
+  value       = { for k, v in azurerm_network_security_group.nsgs : k => v.id }
+}
