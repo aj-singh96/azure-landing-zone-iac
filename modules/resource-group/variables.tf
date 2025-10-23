a156a795feaad5468d0eb74fed7af65d5a6552b8
aj-singh96 <72389457+aj-singh96@users.noreply.github.com>
2025-10-23 17:03:00 +0530

    Update variables.tf

--- modules/resource-group/variables.tf
+++ modules/resource-group/variables.tf
@@ -1,1 +1,15 @@
-# Variables
+variable "name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where the resource group will be created"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the resource group"
+  type        = map(string)
+  default     = {}
+}
