09fe2525f3f3149ac1b18a98f107b4cee8d0cb84
aj-singh96 <72389457+aj-singh96@users.noreply.github.com>
2025-10-23 14:20:11 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,1 +1,24 @@
-# Outputs
+output "role_assignment_ids" {
+  description = "Map of role assignment names to their IDs"
+  value       = { for k, v in azurerm_role_assignment.this : k => v.id }
+}
+
+output "custom_role_assignment_ids" {
+  description = "Map of custom role assignment names to their IDs"
+  value       = { for k, v in azurerm_role_assignment.custom : k => v.id }
+}
+
+output "custom_role_definition_ids" {
+  description = "Map of custom role definition names to their IDs"
+  value       = { for k, v in azurerm_role_definition.custom_roles : k => v.id }
+}
+
+output "custom_role_definition_names" {
+  description = "Map of custom role definition keys to their names"
+  value       = { for k, v in azurerm_role_definition.custom_roles : k => v.name }
+}
+
+output "common_role_definitions" {
+  description = "Map of common Azure built-in role definitions"
+  value       = local.common_roles
+}
