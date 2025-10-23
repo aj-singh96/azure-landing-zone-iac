dc0f2e41bc5d8e417a7694376f09951b1991f747
aj-singh96 <72389457+aj-singh96@users.noreply.github.com>
2025-10-23 14:21:49 +0530

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,1 +1,40 @@
-# Variables
+variable "role_assignments" {
+  description = "Map of role assignments using built-in role names"
+  type = map(object({
+    scope                         = string
+    role_definition_name          = string
+    principal_id                  = string
+    description                   = optional(string)
+    skip_service_principal_aad_check = optional(bool, false)
+  }))
+  default = {}
+}
+
+variable "custom_role_assignments" {
+  description = "Map of role assignments using custom role definition IDs"
+  type = map(object({
+    scope                         = string
+    role_definition_id            = string
+    principal_id                  = string
+    description                   = optional(string)
+    skip_service_principal_aad_check = optional(bool, false)
+  }))
+  default = {}
+}
+
+variable "custom_roles" {
+  description = "Map of custom role definitions to create"
+  type = map(object({
+    name                         = string
+    scope                        = string
+    description                  = string
+    permissions                  = object({
+      actions          = optional(list(string), [])
+      not_actions      = optional(list(string), [])
+      data_actions     = optional(list(string), [])
+      not_data_actions = optional(list(string), [])
+    })
+    assignable_scopes            = list(string)
+  }))
+  default = {}
+}
